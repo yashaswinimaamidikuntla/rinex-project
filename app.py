4843255a5495c8e9a3b6912664b22213638229aa
yashaswinimaamidikuntla <106097313+yashaswinimaamidikuntla@users.noreply.github.com>
2022-07-05 19:26:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-import streamlit as st
-import joblib
-model=joblib.load('twitter-analysis')
-st.title('twitter-analysis CLASSIFIER')
-ip=st.text_input('Enter your text')
-op=model.predict([ip])
-if st.button('Predict'):
-  st.title(op[0])
-            
+import streamlit as st 
+import joblib 
+
+#load the joblib model 
+model_nb = joblib.load('twitter-analysis')
+
+#user input 
+st.title("twitter-analysis CLASSIFIER")
+ip = st.text_input("Enter your text:")
+ 
+op = model_nb.predict([ip])
+if st.button('PREDICT'):
+  st.title(op[0])  #prints the output as spam or ham 
