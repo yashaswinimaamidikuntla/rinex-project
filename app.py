357362c3b00df5da3f05f5f7da3b4cf78943a349
yashaswinimaamidikuntla <106097313+yashaswinimaamidikuntla@users.noreply.github.com>
2022-07-05 19:36:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ model_nb = joblib.load('twitter-analysis')
 #user input 
 st.title("twitter-analysis CLASSIFIER")
 ip = st.text_input("Enter your text:")
- 
+    
 op = model_nb.predict([ip])
 if st.button('PREDICT'):
   st.title(op[0])  #prints the output as spam or ham 
